8057d36abe3ea4747d235fbf335217f3828389f9
c3eyfon <162065693+c3eyfon@users.noreply.github.com>
2025-11-21 11:16:55 +1100

    p2.5 update

---
+++ p2.5/JS/app.js
@@ -0,0 +1,96 @@
+
+// Show Alerts
+function showAlert(msg, className) {
+  const div = document.createElement("div");
+  div.className = `alert alert-${className}`;
+  div.appendChild(document.createTextNode(msg));
+
+  const container = document.querySelector(".container");
+  const main = document.querySelector(".main");
+
+  container.insertBefore(div, main);
+
+  setTimeout(() => document.querySelector(".alert")?.remove(), 3000);
+}
+
+const movieList = new MovieList("movie-list", [
+    { id: 1, title: "Police Story", year: 1985, rating: 8.9 },
+    { id: 2, title: "Hard Boiled", year: 1992, rating: 8.7 },
+    { id: 3, title: "Infernal Affairs", year: 2002, rating: 9.1 },
+    { id: 4, title: "Shaolin Soccer", year: 2001, rating: 8.0 },
+    { id: 5, title: "Ip Man", year: 2008, rating: 8.6 },
+    { id: 6, title: "SPL: Sha Po Lang", year: 2005, rating: 7.8 },
+    { id: 7, title: "Rumble in the Bronx", year: 1995, rating: 7.6},
+    { id: 8, title: "A Better Tomorrow", year: 1986, rating: 8.5},
+    { id: 9, title: "Flash Point", year: 2007, rating: 7.9 },
+    { id: 10, title: "The Raid: Redemption", year: 2011, rating: 8.8 },
+]);
+
+let selectedIndex = null;
+
+// Add function
+document.querySelector("#movie-form").addEventListener("submit", (evt) => {
+    evt.preventDefault();
+
+    const id = document.querySelector("#movieID").value.trim();
+    const title = document.querySelector("#movieTitle").value.trim();
+    const year = document.querySelector("#movieYear").value.trim();
+    const rating = document.querySelector("#movieRating").value.trim();
+
+    if (!id || !title || !year || !rating) {
+        showAlert("Please fill in all fields", "danger");
+        return;
+    }
+
+    if (selectedIndex === null) {
+        movieList.add(id, title, year, rating);
+    } else {
+        movieList.update(selectedIndex, id, title, year, rating);
+        selectedIndex = null;
+    }
+
+    document.querySelector("#movie-form").reset();
+});
+
+
+// Edit & Delete Event Delegation
+document.getElementById("movie-list").addEventListener("click", (evt) => {
+    if (evt.target.classList.contains("edit")) {
+        selectedIndex = evt.target.dataset.index;
+
+        const movie = movieList.movieList[selectedIndex];
+
+        document.querySelector("#movieID").value = movie.id;
+        document.querySelector("#movieTitle").value = movie.title;
+        document.querySelector("#movieYear").value = movie.year;
+        document.querySelector("#movieRating").value = movie.rating;
+    }
+
+    if (evt.target.classList.contains("delete")) {
+        const index = evt.target.dataset.index;
+        movieList.delete(index);
+    }
+});
+
+
+// Sorting button
+document.querySelector("#sortAZ").addEventListener("click", () => movieList.sortAZ());
+document.querySelector("#sortZA").addEventListener("click", () => movieList.sortZA());
+document.querySelector("#sortRateHigh").addEventListener("click", () => movieList.sortRatingHighLow());
+
+// Search button
+document.querySelector("#searchBox").addEventListener("input", (evt) => {
+    const value = evt.target.value.trim();
+
+    if (value === "") {
+        movieList.refresh();
+    } else {
+        movieList.search(value);
+    }
+});
+
+// Refresh button
+
+document.getElementById("refreshBtn").addEventListener("click", () => {
+    movieList.sortIdDsc();
+});
---
+++ p2.5/JS/movie.js
@@ -0,0 +1,130 @@
+class MovieList {
+    constructor(rootId, movieArray) {
+        this.rootId = rootId;
+        this.movieList = movieArray;
+        this.refresh();
+    }
+
+    // remove all table rows
+    removeElements() {
+        const root = document.getElementById(this.rootId);
+        root.innerHTML = "";
+    }
+
+    // generate new row
+    movieRow(index, movie) {
+        const root = document.getElementById(this.rootId);
+
+        const tr = document.createElement("tr");
+
+        tr.innerHTML = `
+            <td>${movie.id}</td>
+            <td>${movie.title}</td>
+            <td>${movie.year}</td>
+            <td>${movie.rating}</td>
+            <td>
+                <button class="btn btn-warning btn-sm edit" data-index="${index}">Edit</button>
+                <button class="btn btn-danger btn-sm delete" data-index="${index}">Delete</button>
+            </td>
+        `;
+
+        root.appendChild(tr);
+    }
+
+    // generate new list
+    genMovieList() {
+        this.removeElements();
+
+        for (let i = 0; i < this.movieList.length; i++) {
+            this.movieRow(i, this.movieList[i]);
+        }
+    }
+
+    // refresh
+    refresh() {
+        this.genMovieList();
+    }
+    
+    // add new movie
+
+    add(id, title, year, rating) {
+        this.movieList.push({
+            id,
+            title,
+            year,
+            rating,
+        });
+
+        this.refresh();
+    }
+
+    // update movie
+
+    update(index, id, title, year, rating) {
+        this.movieList[index] = {
+            id,
+            title,
+            year,
+            rating,
+        };
+
+        this.refresh();
+    }
+
+    //
+
+    delete(index) {
+        this.movieList.splice(index, 1);
+        this.refresh();
+    }
+
+    // Sorting methods
+
+    sortAZ() {
+        this.movieList.sort((a, b) => a.title.localeCompare(b.title));
+        this.refresh();
+    }
+
+    sortZA() {
+        this.movieList.sort((a, b) => b.title.localeCompare(a.title));
+        this.refresh();
+    }
+
+    sortRatingHighLow() {
+        this.movieList.sort((a, b) => b.rating - a.rating);
+        this.refresh();
+    }
+
+     sortIdDsc() {
+        this.movieList.sort((a, b) => a.id - b.id);
+        this.refresh();
+    }
+
+    // Search method
+
+search(searchString) {
+    const searchInput = searchString.toLowerCase();
+    const filteredList = [];
+
+    for (let movie of this.movieList) {
+        const idMatch = movie.id.toString().includes(searchInput);
+        const titleMatch = movie.title.toLowerCase().includes(searchInput);
+
+        if (idMatch || titleMatch) {
+            filteredList.push(movie);
+        }
+    }
+
+    showAlert(`${filteredList.length} result(s) found`, 
+        filteredList.length > 0 ? "success" : "danger"
+    );
+
+    this.removeElements();
+
+    for (let i = 0; i < filteredList.length; i++) {
+        const movie = filteredList[i];
+        this.movieRow(i, movie);
+    }
+}
+
+}
